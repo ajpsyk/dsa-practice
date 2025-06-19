54365715e4ba9b7d7f81a29d9fd5eb52717a1357
ajpsyk <ajpsyk@gmail.com>
2025-06-19 16:43:54 -0700

    26 solution

--- Arrays/26.Remove-Duplicates-from-Sorted-Array.py
+++ Arrays/26.Remove-Duplicates-from-Sorted-Array.py
@@ -1,6 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # nums = [0,0,1,1,1,2,2,3,3,4]
+        # solution where we can iterate once?
+        # first pointer and "counter" pointer on the index 0 number
+        # second "counter" pointer on the index 1 number
+        # if different, advance counter, make counter value = interator value
+        # advance iterator
         k = 0
         i = 0
         while i < len(nums):
@@ -8,9 +13,4 @@ class Solution:
                 k = k + 1
                 nums[k] = nums[i]
             i = i + 1
-        # solution where we can iterate once?
-        # first pointer and "counter" pointer on the index 0 number
-        # second "counter" pointer on the index 1 number
-        # if different, advance counter, make counter value = interator value
-        # advance iterator
         return k + 1
