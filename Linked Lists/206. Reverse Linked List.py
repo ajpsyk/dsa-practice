7693ca486c3a62930a3d4fbc8dbf750d79c6c833
ajpsyk <ajpsyk@gmail.com>
2025-06-20 13:57:51 -0700

    reverse linked list solution

--- Linked Lists/206. Reverse Linked List.py
+++ Linked Lists/206. Reverse Linked List.py
@@ -5,7 +5,32 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        prev = None
+        curr = head
+        next = head.next
+
+        while next:
+            curr.next = prev
+            prev = curr
+            curr = next
+            next = next.next
+        
+        curr.next = prev
+        return curr
+
 
 # Recursive
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        return self.reverseListHelper(head, None, head.next)
+
+    def reverseListHelper(self, curr: Optional[ListNode], prev: Optional[ListNode], next: Optional[ListNode]) -> Optional[ListNode]:
+        curr.next = prev
+        if not next:
+            return curr
+        return self.reverseListHelper(next, curr, next.next)
